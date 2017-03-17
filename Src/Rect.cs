8ae22f905027e9bae2fb585428e4cba08841bc59
Discosultan <j2v@neti.ee>
2017-03-17 09:28:06 +0200

    [Src] Move ClearRect to CommandBuffer.cs

--- Src/Rect.cs
+++ Src/Rect.cs
@@ -182,37 +182,4 @@ namespace VulkanCore
         /// </returns>
         public static bool operator !=(Rect3D left, Rect3D right) => !left.Equals(right);
     }
-
-    /// <summary>
-    /// Structure specifying a clear rectangle.
-    /// </summary>
-    [StructLayout(LayoutKind.Sequential)]
-    public struct ClearRect
-    {
-        /// <summary>
-        /// The two-dimensional region to be cleared.
-        /// </summary>
-        public Rect2D Rect;
-        /// <summary>
-        /// The first layer to be cleared.
-        /// </summary>
-        public int BaseArrayLayer;
-        /// <summary>
-        /// The number of layers to clear.
-        /// </summary>
-        public int LayerCount;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ClearRect"/> structure.
-        /// </summary>
-        /// <param name="rect">The two-dimensional region to be cleared.</param>
-        /// <param name="baseArrayLayer">The first layer to be cleared.</param>
-        /// <param name="layerCount">The number of layers to clear.</param>
-        public ClearRect(Rect2D rect, int baseArrayLayer, int layerCount)
-        {
-            Rect = rect;
-            BaseArrayLayer = baseArrayLayer;
-            LayerCount = layerCount;
-        }
-    }
 }
