f5c08b53ae09db63ba16860acbe52b2f0f333c76
discosultan <jaanusvarus@gmail.com>
2017-10-07 12:21:49 +0200

    Remove Rect3D

--- Tests/RectTest.cs
+++ Tests/RectTest.cs
@@ -19,20 +19,5 @@ namespace VulkanCore.Tests
             Assert.True(val1 != val2);
             Assert.NotEqual(val1.GetHashCode(), val2.GetHashCode());
         }
-
-        [Fact]
-        public void Rect3DEquals()
-        {
-            var val1 = new Rect3D(0, 1, 2, 3, 4, 5);
-            var val2 = new Rect3D(6, 7, 8, 9, 10, 11);
-
-            Assert.True(val1.Equals(val1));
-            Assert.False(val1.Equals(val2));
-            Assert.True(val1 == val1);
-            Assert.False(val1 == val2);
-            Assert.False(val1 != val1);
-            Assert.True(val1 != val2);
-            Assert.NotEqual(val1.GetHashCode(), val2.GetHashCode());
-        }
     }
 }
