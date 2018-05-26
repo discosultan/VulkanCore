ce9e00b9eec7dab4ea3b95e8d89e796d6b3aa070
Bastian Blokland <bastianblokland@gmail.com>
2018-05-27 01:24:12 +0300

    Created mac-os versions of the sample projects

---
+++ Samples/MacOS/01-ClearScreen/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Reflection;
+
+namespace VulkanCore.Samples.ClearScreen
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (var window = new MacOSWindow(
+                Assembly.GetExecutingAssembly().GetName().Name,
+                new ClearScreenApp()))
+            {
+                window.Initialize();
+                window.Run();
+            }
+        }
+    }
+}
---
+++ Samples/MacOS/03-TexturedCube/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Reflection;
+
+namespace VulkanCore.Samples.TexturedCube
+{
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (var window = new MacOSWindow(
+                Assembly.GetExecutingAssembly().GetName().Name,
+                new TexturedCubeApp()))
+            {
+                window.Initialize();
+                window.Run();
+            }
+        }
+    }
+}
