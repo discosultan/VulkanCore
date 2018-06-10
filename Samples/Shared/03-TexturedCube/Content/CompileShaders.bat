9892d998772e53afc853c80210f0f874d076bde2
discosultan <jaanusvarus@gmail.com>
2018-06-10 21:34:21 +0200

    Add content files to MacOS samples

--- Samples/Shared/03-TexturedCube/Content/CompileShaders.bat
+++ Samples/Shared/03-TexturedCube/Content/CompileShaders.bat
@@ -1,7 +1,7 @@
 REM Make the directory batch file resides in as the working directory.
 pushd %~dp0
 
-glslangvalidator -V shader.vert -o shader.vert.spv
-glslangvalidator -V shader.frag -o shader.frag.spv
+glslangvalidator -V Shader.vert -o Shader.vert.spv
+glslangvalidator -V Shader.frag -o Shader.frag.spv
 
 popd
--- Samples/Shared/04-ComputeParticles/Content/CompileShaders.bat
+++ Samples/Shared/04-ComputeParticles/Content/CompileShaders.bat
@@ -1,8 +1,8 @@
 REM Make the directory batch file resides in as the working directory.
 pushd %~dp0
 
-glslangvalidator -V shader.vert -o shader.vert.spv
-glslangvalidator -V shader.frag -o shader.frag.spv
-glslangvalidator -V shader.comp -o shader.comp.spv
+glslangvalidator -V Shader.vert -o Shader.vert.spv
+glslangvalidator -V Shader.frag -o Shader.frag.spv
+glslangvalidator -V Shader.comp -o Shader.comp.spv
 
 popd
